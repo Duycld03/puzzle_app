7a9a8b96a3b8c02d6c2e5ca37cbb030cce05fbdc
Duy <55023899+Duycld03@users.noreply.github.com>
2026-08-22 22:27:23 +0700

    refactor: optimize PlayBloc - fix state mutation and improve logic separation

--- lib/blocs/play/play_state.dart
+++ lib/blocs/play/play_state.dart
@@ -1,25 +1,26 @@
 part of 'play_bloc.dart';
 
 class PlayState {
-  int? id;
-  int life;
-  List<Question> questions;
-  Question? currentQuestion;
-  String? answer;
-  bool isGameOver;
-  bool isCorrect;
-  bool isShow;
-  bool isTimeout;
-  List<String> options;
-  String? fillOption;
-  TextEditingController fillOptionCtrl;
-  CountDownController countdownCtrl;
-  bool isLoaded = false;
-  int durationTimeout;
-  int totalCorrectQuestion;
-  int totalIncorrectQuestion;
+  final int id;
+  final int life;
+  final List<Question> questions;
+  final Question? currentQuestion;
+  final String? answer;
+  final bool isGameOver;
+  final bool isCorrect;
+  final bool isShow;
+  final bool isTimeout;
+  final List<String> options;
+  final String fillOption;
+  final TextEditingController fillOptionCtrl;
+  final CountDownController countdownCtrl;
+  final bool isLoaded;
+  final int durationTimeout;
+  final int totalCorrectQuestion;
+  final int totalIncorrectQuestion;
 
   PlayState({
+    required this.id,
     required this.isGameOver,
     required this.life,
     required this.questions,
@@ -35,13 +36,8 @@ class PlayState {
     required this.totalCorrectQuestion,
     required this.totalIncorrectQuestion,
     this.currentQuestion,
-    this.id,
     this.answer,
-  }) {
-    if (questions.isNotEmpty) {
-      currentQuestion = questions[id ?? 0];
-    }
-  }
+  });
 
   PlayState copyWith({
     int? id,
@@ -61,36 +57,35 @@ class PlayState {
     CountDownController? countdownCtrl,
     int? totalCorrectQuestion,
     int? totalIncorrectQuestion,
-  }) =>
-      PlayState(
-        id: id ?? this.id,
-        isGameOver: isGameOver ?? this.isGameOver,
-        life: life ?? this.life,
-        questions: questions ?? this.questions,
-        currentQuestion: currentQuestion ?? this.currentQuestion,
-        answer: answer ?? this.answer,
-        options: options ?? this.options,
-        isLoaded: isLoaded ?? this.isLoaded,
-        fillOption: fillOption ?? this.fillOption,
-        fillOptionCtrl: fillOptionCtrl ?? this.fillOptionCtrl,
-        isCorrect: isCorrect ?? this.isCorrect,
-        isShow: isShow ?? this.isShow,
-        isTimeout: isTimeout ?? this.isTimeout,
-        durationTimeout: durationTimeout ?? this.durationTimeout,
-        countdownCtrl: countdownCtrl ?? this.countdownCtrl,
-        totalCorrectQuestion: totalCorrectQuestion ?? this.totalCorrectQuestion,
-        totalIncorrectQuestion:
-            totalIncorrectQuestion ?? this.totalIncorrectQuestion,
-      );
+  }) {
+    final newId = id ?? this.id;
+    final newLife = life ?? this.life;
+    final newIsGameOver = isGameOver ?? this.isGameOver;
+    final newQuestions = questions ?? this.questions;
+    final newCurrentQuestion = currentQuestion ?? (newQuestions.isEmpty ? null : newQuestions[newId]);
 
-  get isValid => fillOption!.isNotEmpty;
-
-  minusLife() {
-    life--;
-    if (life == 0) {
-      isGameOver = true;
-    }
+    return PlayState(
+      id: newId,
+      isGameOver: newIsGameOver,
+      life: newLife,
+      questions: newQuestions,
+      currentQuestion: newCurrentQuestion,
+      answer: answer ?? this.answer,
+      options: options ?? this.options,
+      isLoaded: isLoaded ?? this.isLoaded,
+      fillOption: fillOption ?? this.fillOption,
+      fillOptionCtrl: fillOptionCtrl ?? this.fillOptionCtrl,
+      isCorrect: isCorrect ?? this.isCorrect,
+      isShow: isShow ?? this.isShow,
+      isTimeout: isTimeout ?? this.isTimeout,
+      durationTimeout: durationTimeout ?? this.durationTimeout,
+      countdownCtrl: countdownCtrl ?? this.countdownCtrl,
+      totalCorrectQuestion: totalCorrectQuestion ?? this.totalCorrectQuestion,
+      totalIncorrectQuestion: totalIncorrectQuestion ?? this.totalIncorrectQuestion,
+    );
   }
+
+  bool get isValid => fillOption.isNotEmpty;
 }
 
 class PlayInitial extends PlayState {
